e1b8bce15c0460cbc461b4e501cceb273743a7e8
Strikingwolf <Strikingwolf2012@gmail.com>
2014-11-16 15:49:13 -0600

    Made factorial prettier

--- src/factorial/factorial.hs
+++ src/factorial/factorial.hs
@@ -1,6 +1,7 @@
 module Main where
 
-factorial n = if n == 0 then 1 else n * factorial (n - 1)
+factorial n | n < 2 = 1
+factorial n = n * factorial (n-1)
 
 main = do putStrLn "What number do you want to find the factorial of?"
           x <- readLn
