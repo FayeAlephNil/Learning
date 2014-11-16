13af4cf9f1435f2e3b4573fefbce00c86d9c2d7e
Strikingwolf <Strikingwolf2012@gmail.com>
2014-11-16 16:30:12 -0600

    divisible

---
+++ src/divisible/divisible.hs
@@ -0,0 +1,10 @@
+module Main where
+
+getAllDivisibles n = [x | x <- [1..], x `mod` n == 0]
+getSomeDivisibles y x = take y (getAllDivisibles x)
+
+main = do putStrLn "What number to see if divisible by"
+          x <- readLn
+          putStrLn "How many do you want?"
+          y <- readLn
+          print (getSomeDivisibles y x)
