56516e45ace475c4a0e38872c178da80250cfd89
William Jackson <Strikingwolf2012@gmail.com>
2015-02-11 17:48:27 -0600

    Update core_test.clj

--- test/general_clojure/core_test.clj
+++ test/general_clojure/core_test.clj
@@ -3,5 +3,5 @@
             [general-clojure.core :refer :all]))
 
 (deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+  (testing "Always returns true because we aren't doing anything in core yet :P"
+    true))
