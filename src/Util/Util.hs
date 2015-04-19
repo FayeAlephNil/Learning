57cdee3b6ec1cb51f3e94d52a9b7faa97996b07b
Strikingwolf <Strikingwolf2012@gmail.com>
2015-04-19 10:12:06 -0500

    Some more Util functions

--- src/Util/Util.hs
+++ src/Util/Util.hs
@@ -4,3 +4,12 @@ combine :: [a] -> [(a, a)]
 combine [] = []
 combine [x] = [(x,x)]
 combine (x:xs) = (x, (last xs)):(combine (init xs))
+
+groupsOf :: Int -> [a] -> [[a]]
+groupsOf 0 _ = undefined
+groupsOf _ [] = []
+groupsOf n xs = take n xs : groupsOf n (drop n xs)
+
+split :: [a] -> [[a]]
+split [] = []
+split xs = groupsOf ((length xs) `quot` 2) xs
