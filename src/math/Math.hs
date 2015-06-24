4bf38be7d010005fc5eee0f024a60118bf01a17e
Striking <Strikingwolf2012@gmail.com>
2015-06-23 23:05:59 -0500

    Add fibonacci stuff
    * period generator and periods for lucas and fib
    * fib_seq generator
    * lucas numbers

--- src/math/Math.hs
+++ src/math/Math.hs
@@ -20,15 +20,32 @@ factorial n
 	| n >= 0 = product [1..n]
 	| otherwise = error "You passed a number less than 0 to the factorial function"
 
--- Fibonacci
+-- Fibonacci Sequences
 
-fib :: Int -> Integer
-fib n
-	| n < 0     = nega_fibs !! (-n)
-	| otherwise = fibs !! n
+fib_seq :: Integer -> Integer -> (Int -> Integer)
+fib_seq n m = (\x -> if x < 0 then nega_list !! (-x) else list !! x)
 	where
-		fibs = (0 : 1 : zipWith (+) fibs (tail fibs)) :: [Integer]
-		nega_fibs = (0 : 1 : zipWith (-) nega_fibs (tail nega_fibs)) :: [Integer]
+		list = (n : m : zipWith (+) list (tail list)) :: [Integer]
+		nega_list = (n : (m-n) : zipWith (-) nega_list (tail nega_list)) :: [Integer]
+
+fib_period_gen :: (Int -> Integer) -> Integer -> (Int -> Integer)
+fib_period_gen f n = (\x -> gen x `mod` n)
+	where
+		gen = fib_seq (f 0 `mod` n) (f 1 `mod` n)
+
+fib = fib_seq 0 1
+
+fibs = map fib [0..]
+nega_fibs = map fib [0,(-1)..]
+
+fib_period = fib_period_gen fib
+
+lucas = fib_seq 2 1
+
+lucases = map lucas [0..]
+nega_lucases = map lucas [0,(-1)..]
+
+lucas_period = fib_period_gen lucas
 
 -- Factors
 
