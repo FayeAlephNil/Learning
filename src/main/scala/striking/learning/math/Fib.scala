ae192dd7f4a37f1f49464874c8ff2c13ffe1b141
Striking <Strikingwolf2012@gmail.com>
2015-07-21 13:53:39 -0500

    I think this memoized it?
    Signed-off-by: Striking <Strikingwolf2012@gmail.com>

--- src/main/scala/striking/learning/math/Fib.scala
+++ src/main/scala/striking/learning/math/Fib.scala
@@ -1,23 +1,38 @@
 package striking.learning.math
 
+import striking.learning.Implicits
+
 import scala.collection.LinearSeq
+import Implicits._
 
 class Fib(n: BigInt, m: BigInt) {
-	def get(x:Int): BigInt = {
-		def new_fibs(x:Int):(BigInt,BigInt) = if (x == 0) (n, m) else {
-			val (a,b) = new_fibs(x/2)
-			val p = (2*b+a)*a
-			val q = a*a + b*b
-			if(x % 2 == 0) (p,q) else (p+q,p)
-		}
-		if (x < 0) {
-			val n = -x
-			BigInt(-1).pow(n+1) * new_fibs(n)._1
-		} else {
-			new_fibs(x)._1
-		}
+	private def new_fibs(x:Int):(BigInt,BigInt) = if (x == 0) (n, m) else if (x < 0) {
+		val n = -x
+		val result = BigInt(-1).pow(n + 1) * get(n)
+		(result, 0)
+	} else {
+		val (a,b) = getTuple(x/2)
+		val p = (2*b+a)*a
+		val q = a*a + b*b
+		if(x % 2 == 0) (p,q) else (p+q,p)
 	}
 
-	def list: LinearSeq[BigInt] = Reference.whole.map(get)
-	def negaList: LinearSeq[BigInt] = Reference.negative.map(get)
+	private def getTuple: (Int => (BigInt, BigInt)) = listTuple.andNega(negaListTuple)
+
+	def get: (Int => BigInt) = list.andNega(negaList)
+
+	private val listTuple: LinearSeq[(BigInt, BigInt)] = Reference.whole.map(new_fibs)
+	private val negaListTuple: LinearSeq[(BigInt, BigInt)] = Reference.negative.map(new_fibs)
+
+	def list: LinearSeq[BigInt] = listTuple.map(tuple => tuple._1)
+	def negaList: LinearSeq[BigInt] = negaListTuple.map(tuple => tuple._1)
+
+	def regenWithGen(regen: Fib => (Int, Int)): Fib = {
+		val (a, b) = regen(this)
+		new Fib(a, b)
+	}
+
+	def regenWithIndex(a: Int, b: Int): Fib = {
+
+	}
 }
