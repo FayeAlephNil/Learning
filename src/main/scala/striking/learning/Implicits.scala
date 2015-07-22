60c2a58ffd29f54e50eb60da1f5c676ecbaffc38
Striking <Strikingwolf2012@gmail.com>
2015-07-21 21:19:35 -0500

    Add digits stuff
    * add unDigits to SeqIntImplicit
    * add the hash method
    * add neg
    * add digitsRev
    * add digits
    * add sumDigits
    * add digitSum
    * add minDigitSumList
    * add minDigitSum
    
    Signed-off-by: Striking <Strikingwolf2012@gmail.com>

--- src/main/scala/striking/learning/Implicits.scala
+++ src/main/scala/striking/learning/Implicits.scala
@@ -1,5 +1,7 @@
 package striking.learning
 
+import scala.annotation.tailrec
+
 object Implicits {
 	implicit class SeqImplicit[A](xs: Seq[A]) {
 		def andNega(negaList: Seq[A]): (Int => A) = {
@@ -10,6 +12,61 @@ object Implicits {
 					xs.apply(x)
 				}
 			}
+		}
+	}
+
+	implicit class SeqIntImplicit(xs: Seq[Int]) {
+		def unDigits: Int = {
+			xs.reduceLeft((a, b) => {
+				(a * 10) + b
+			})
+		}
+	}
+
+	implicit class IntImplicit(x: Int) {
+		def hash(y: Int): Int = {
+			((x-1) % y) + 1
+		}
+
+		def neg: Int = {
+			if (x < 0) {
+				-1
+			} else {
+				1
+			}
+		}
+
+		def digitsRev: List[Int] = {
+			val rest: Int = x / 10
+			val lastDigit: Int = x % 10
+			x match {
+				case 0 => List[Int]()
+				case _ => lastDigit :: rest.digitsRev
+			}
+		}
+
+
+		def digits: List[Int] = {
+			val digitsRev = x.digitsRev
+			digitsRev.reverse
+		}
+
+
+		def sumDigits: Int = {
+			x.digitsRev.sum
+		}
+
+		def digitSum: Int = {
+			x.neg * x hash 9
+		}
+
+		def minDigitSumList: List[Int] = {
+			def loop: Stream[Int] = (x.neg * 9) #:: loop
+			x.digitSum :: loop.take(Math.abs(x/9)).toList
+		}
+
+		def minDigitSum: Int = {
+			x.minDigitSumList.unDigits
 		}
 	}
 }
--- src/main/scala/striking/learning/Learning.scala
+++ src/main/scala/striking/learning/Learning.scala
@@ -1,14 +1,10 @@
 package striking.learning
 
-import striking.learning.math.MathRef
+import Implicits._
 
 object Learning extends App {
-	println("Hello World!")
-	new Timer(timer => println("1000 milliseconds have passed"), 1000).start
-	new Timer(timer => println("2000 milliseconds have passed"), 2000).start
-	new Timer(timer => {
-		println("3000 milliseconds have passed, Adios")
-		timer.stop
-	}, 3000).start
-	MathRef.FibNumbers.pisano(7).list.take(32).foreach(println)
+	println(10.minDigitSum)
+	println(233.digits.product)
+	println(371.digitSum)
+	println(21.sumDigits)
 }
