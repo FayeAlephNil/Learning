17a4721f1e0434c189ff301cc6e5501c0f10b524
Striking <Strikingwolf2012@gmail.com>
2015-07-22 10:51:38 -0500

    Testing factorial, even, zipWith, and stern
    Signed-off-by: Striking <Strikingwolf2012@gmail.com>

--- src/main/scala/striking/learning/Learning.scala
+++ src/main/scala/striking/learning/Learning.scala
@@ -1,7 +1,13 @@
 package striking.learning
 
 import striking.learning.math.MathImps._
+import Implicits._
+import striking.learning.math.Stern
 
 object Learning extends App {
+	(1 :: 2 :: 3 :: List[Int]()).zipWith[Int, Int]((x, y) => x + y, 4 :: 5 :: 6 :: List[Int]()).foreach(println)
 	412.factorization.foreach(println)
+	println(5.factorial)
+	println(0.factorial)
+	Stern.sterns.take(10).foreach(println)
 }
