e6d127dac3b3160eeefdfbdd86366445857c91f1
Striking <Strikingwolf2012@gmail.com>
2015-07-24 11:43:42 -0500

    Rework Stream system in MathRef
    Signed-off-by: Striking <Strikingwolf2012@gmail.com>

--- src/main/scala/striking/learning/math/MathRef.scala
+++ src/main/scala/striking/learning/math/MathRef.scala
@@ -1,16 +1,28 @@
 package striking.learning.math
 
 object MathRef {
-	def whole: Stream[Int] = {
-		def loop(v: Int): Stream[Int] = v #:: loop(v + 1)
-		loop(0)
+	def numStreamBig(start: BigInt, second: BigInt, f: (BigInt, BigInt) => BigInt): Stream[BigInt] = {
+		def loop(v: BigInt, last: BigInt): Stream[BigInt] = {
+			v #:: loop(f(v, last), v)
+		}
+		start #:: loop(second, start)
 	}
 
-	def negative: Stream[Int] = {
-		def loop(v: Int): Stream[Int] = v #:: loop(v - 1)
-		loop(0)
+	def numStream(start: Int, second: Int, f: (Int, Int) => Int): Stream[Int] = {
+		def loop(v: Int, last: Int): Stream[Int] = {
+			v #:: loop(f(v, last), v)
+		}
+		start #:: loop(second, start)
 	}
 
-	def Lucas = Fib(2, 1)
-	def FibNumbers = Fib(0, 1)
+	def natural: Stream[Int] = numStream(1, 2, (x: Int, y: Int) => x + 1)
+
+	def whole: Stream[Int] = numStream(0, 1, (x: Int, y: Int) => x + 1)
+
+	def negative0: Stream[Int] = numStream(-1, -2, (x: Int, y: Int) => x - 1)
+
+	def negative: Stream[Int] = numStream(0, -1, (x: Int, y: Int) => x - 1)
+
+	def Lucas = new Fib(2, 1)
+	def FibNumbers = new Fib(0, 1)
 }
