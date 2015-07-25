62fe637d24365dcd4e31bd078ca6f8f8b1b0e798
Striking <Strikingwolf2012@gmail.com>
2015-07-25 01:40:24 -0500

    Add some Seq to Stern :P
    Signed-off-by: Striking <Strikingwolf2012@gmail.com>

--- src/main/scala/striking/learning/math/Stern.scala
+++ src/main/scala/striking/learning/math/Stern.scala
@@ -3,9 +3,11 @@ package striking.learning.math
 import MathImps._
 
 import scala.annotation.tailrec
-import scala.collection.LinearSeq
+import scala.collection.{GenTraversableOnce, LinearSeq}
 
-object Stern {
+class Stern(_modifier: Stern.Modifier = Stern.defaultModifier) {
+	def modifier = _modifier
+
 	@tailrec
 	private def _stern(acc: Int, x: Int): Int = {
 		x match {
@@ -27,5 +29,34 @@ object Stern {
 		sterns.apply(x)
 	}
 
-	val sterns: LinearSeq[Int] = MathRef.whole.map(_stern(0, _))
+	val _sterns: LinearSeq[Int] = MathRef.whole.map(_stern(0, _))
+
+	def sterns: LinearSeq[Int] = modifier(_sterns)
+
+	def flatMap(f: Int => GenTraversableOnce[Int]): Stern = {
+		def nextModifier: Stern.Modifier = { (aList) =>
+			modifier(aList).flatMap(f)
+		}
+		new Stern(nextModifier)
+	}
+
+	def map(f: Int => Int): Stern = {
+		def nextModifier: Stern.Modifier = { (aList) =>
+			modifier(aList).map(f)
+		}
+		new Stern(nextModifier)
+	}
+
+	def filter(f: Int => Boolean): Stern = {
+		def nextModifier: Stern.Modifier = { (aList) =>
+			modifier(aList).filter(f)
+		}
+		new Stern(nextModifier)
+	}
+}
+
+object Stern {
+	type Modifier = LinearSeq[Int] => LinearSeq[Int]
+
+	val defaultModifier: Modifier = identity
 }
