0d31ed3dde0f9e069b1c0c5ad6ac4ed877603c4f
Striking <Strikingwolf2012@gmail.com>
2015-07-25 14:04:34 -0500

    Further testing
    Signed-off-by: Striking <Strikingwolf2012@gmail.com>

--- src/main/scala/striking/learning/Learning.scala
+++ src/main/scala/striking/learning/Learning.scala
@@ -1,8 +1,8 @@
 package striking.learning
 
-import striking.learning.math.{Cobweb, MathRef}
+import striking.learning.math.{LogisticMap, Cobweb, MathRef}
 import striking.learning.math.MathImps._
 
 object Learning extends App {
-	MathRef.BasicLogistic.cobweb.map(t => (t._1 * 2, t._2 * 2)).list.take(10).foreach(println)
+	new LogisticMap(4, 0.2).cobweb.map(t => (t._1 * 2, t._2 * 2)).list.take(10).foreach(println)
 }
