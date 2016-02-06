e9304e680f8b009750b5e7580af0d87d3161a2a0
Striking <Strikingwolf2012@gmail.com>
2016-02-06 09:39:32 -0600

    More matrix work
    size, transpose, add, zipWithMatrix, zipMatrix, and Show

--- haskell/src/math/Matrix.hs
+++ haskell/src/math/Matrix.hs
@@ -1,21 +1,27 @@
 module Math.Matrix
 ( Matrix
 , create
+, size
 , rows
 , columns
+, transpose
+, add
 , scalarMult
 , matrixMult
+, zipWithMatrix
+, zipMatrix
 ) where
 
-import Data.List
+import qualified Data.List as List
+import qualified Util.Util as Util
 
-data Matrix a = Create [[a]] [[a]] deriving (Show, Read, Eq)
+data Matrix a = Create [[a]] [[a]] deriving (Eq)
 
 -- (Num a) is a scalar of a matrix
 
 -- Creates a matrix taking each part of an array to be a row
 create :: [[a]] -> Matrix a
-create rs = Create rs (transpose rs)
+create rs = Create rs (List.transpose rs)
 
 size :: Matrix a -> (Int, Int)
 size (Create rs cs) = (length rs, length cs)
@@ -25,6 +31,14 @@ rows (Create rs _) = rs
 
 columns :: Matrix a -> [[a]]
 columns (Create _ cs) = cs
+
+transpose :: Matrix a -> Matrix a
+transpose (Create rs cs) = Create cs rs
+
+add :: (Num a) => Matrix a -> Matrix a -> Maybe (Matrix a)
+add a b
+  | size a == size b = Just $ zipWithMatrix (+) a b
+  | otherwise = Nothing
 
 scalarMult :: (Num a) => a -> Matrix a -> Matrix a
 scalarMult scalar = fmap (* scalar)
@@ -43,7 +57,20 @@ rowColumnProd :: (Num a)
   -> a -- Scalar result
 rowColumnProd r c = sum $ zipWith (*) r c
 
+zipWithMatrix :: (a -> b -> c) -> Matrix a -> Matrix b -> Matrix c
+zipWithMatrix f (Create as _) (Create bs _) = create $ [zipWith f a b | (a, b) <- (zip as bs)]
+
+zipMatrix :: Matrix a -> Matrix b -> Matrix (a, b)
+zipMatrix = zipWithMatrix Util.tuple
+
+
 instance Functor Matrix where
   fmap f (Create rs cs) = create rs'
     where
       rs' = map (map f) rs
+
+instance (Show a) => Show (Matrix a) where
+  show (Create rs _) = tail $ show' rs ""
+    where
+      show' [] acc = acc
+      show' (a : as) acc = show' as (acc ++ "\n" ++ show a)
