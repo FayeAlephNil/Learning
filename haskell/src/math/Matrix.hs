a0af9ba7a170db6517966b2d273dd69778334f3f
Striking <Strikingwolf2012@gmail.com>
2016-02-06 13:15:45 -0600

    Matrix Applicative

--- haskell/src/math/Matrix.hs
+++ haskell/src/math/Matrix.hs
@@ -1,27 +1,38 @@
 module Math.Matrix
 ( Matrix
+, Matrices
 , create
+, empty
 , size
 , rows
 , columns
+, items
 , transpose
 , add
 , scalarMult
 , matrixMult
 , zipWithMatrix
 , zipMatrix
+, concatMatrix
 ) where
 
 import qualified Data.List as List
 import qualified Util.Util as Util
 
+import qualified Control.Applicative as App
+import qualified Data.Foldable as Fold
+
 data Matrix a = Create [[a]] [[a]] deriving (Eq)
+type Matrices a = [Matrix a]
 
 -- (Num a) is a scalar of a matrix
 
 -- Creates a matrix taking each part of an array to be a row
 create :: [[a]] -> Matrix a
 create rs = Create rs (List.transpose rs)
+
+empty :: Matrix a
+empty = create []
 
 size :: Matrix a -> (Int, Int)
 size (Create rs cs) = (length rs, length cs)
@@ -31,6 +42,9 @@ rows (Create rs _) = rs
 
 columns :: Matrix a -> [[a]]
 columns (Create _ cs) = cs
+
+items :: Matrix a -> [a]
+items = concat . rows
 
 transpose :: Matrix a -> Matrix a
 transpose (Create rs cs) = Create cs rs
@@ -58,19 +72,25 @@ rowColumnProd :: (Num a)
 rowColumnProd r c = sum $ zipWith (*) r c
 
 zipWithMatrix :: (a -> b -> c) -> Matrix a -> Matrix b -> Matrix c
-zipWithMatrix f (Create as _) (Create bs _) = create $ [zipWith f a b | (a, b) <- (zip as bs)]
+zipWithMatrix f (Create as _) (Create bs _) = create [zipWith f a b | (a, b) <- zip as bs]
 
 zipMatrix :: Matrix a -> Matrix b -> Matrix (a, b)
 zipMatrix = zipWithMatrix Util.tuple
 
+concatMatrix :: Matrix (Matrix a) -> Matrix a
+concatMatrix = create . concatMap rows . items
+
+instance Foldable Matrix where
+  foldr f acc (Create rs _) = foldr f acc $ concat rs
 
 instance Functor Matrix where
   fmap f (Create rs cs) = create rs'
     where
       rs' = map (map f) rs
 
+instance Applicative Matrix where
+  pure a = Create [[a]] [[a]]
+  a <*> b = concatMatrix $ fmap (`fmap` b) a
+
 instance (Show a) => Show (Matrix a) where
-  show (Create rs _) = tail $ show' rs ""
-    where
-      show' [] acc = acc
-      show' (a : as) acc = show' as (acc ++ "\n" ++ show a)
+  show (Create rs _) = tail $ foldl (\ acc a -> acc ++ "\n" ++ show a) "" rs
