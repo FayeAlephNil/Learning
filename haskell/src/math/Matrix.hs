2f9f0405c22cf873e7532ec8c1667bbed2069417
Striking <Strikingwolf2012@gmail.com>
2016-02-05 20:55:37 -0600

    Matrix Multiplication

--- haskell/src/math/Matrix.hs
+++ haskell/src/math/Matrix.hs
@@ -4,6 +4,7 @@ module Math.Matrix
 , rows
 , columns
 , scalarMult
+, matrixMult
 ) where
 
 import Data.List
@@ -16,6 +17,9 @@ data Matrix a = Create [[a]] [[a]] deriving (Show, Read, Eq)
 create :: [[a]] -> Matrix a
 create rs = Create rs (transpose rs)
 
+size :: Matrix a -> (Int, Int)
+size (Create rs cs) = (length rs, length cs)
+
 rows :: Matrix a -> [[a]]
 rows (Create rs _) = rs
 
@@ -24,6 +28,20 @@ columns (Create _ cs) = cs
 
 scalarMult :: (Num a) => a -> Matrix a -> Matrix a
 scalarMult scalar = fmap (* scalar)
+
+matrixMult :: (Num a) => Matrix a -> Matrix a -> Maybe (Matrix a)
+matrixMult a b
+  | snd (size a) == fst (size b) = Just $ create [map (rowColumnProd r) cs | r <- rs]
+  | otherwise = Nothing
+  where
+    rs = rows a
+    cs = columns b
+
+rowColumnProd :: (Num a)
+  => [a] -- Row
+  -> [a] -- Column
+  -> a -- Scalar result
+rowColumnProd r c = sum $ zipWith (*) r c
 
 instance Functor Matrix where
   fmap f (Create rs cs) = create rs'
