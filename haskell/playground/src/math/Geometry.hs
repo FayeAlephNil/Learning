07383dc6906d9308d41fee1fecba4eb192c02611
Striking <Strikingwolf2012@gmail.com>
2016-06-08 19:31:44 -0500

    Geometry
    Adds cool stuff, like figures yo

--- haskell/playground/src/math/Geometry.hs
+++ haskell/playground/src/math/Geometry.hs
@@ -1,14 +1,86 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+
 module Math.Geometry where
 
-type Point = [Int]
+import Delude
+
+tau :: Double
+tau = 2 * pi
+
+type Point = (Double, Double)
 type Line = (Point, Point)
-type Shape = [Line]
 
+-- Define distance and some point functions
 
-point2D :: a -> a -> Point2D a
-point2D a b = Values a (Values b EmptyPoint)
+pythag :: (Floating a) => a -> a -> a
+pythag a b = sqrt (a ** 2) + (b ** 2)
 
-let a = Matrix.create [[3,2], [4,5]]
-let b = Matrix.create [[\x -> x + 1, \x -> x * x], [\x -> x * 2, \x -> x * 3]]
-let c = [a, Matrix.transpose a, fmap (^ 2) a]
-let d = \x -> fmap ($ x) b
+euDist :: Point -> Point -> Double
+euDist (x, y) (x', y') = pythag (x - x') (y - y')
+
+rotate :: Point -> Double -> Point -> Point
+rotate (x, y) theta center@(ox, oy) = (c * xdiff - s * ydiff + ox, s * xdiff + c * ydiff + oy)
+  where
+    xdiff = x - ox
+    ydiff = y - oy
+    c = cos theta
+    s = sin theta
+
+rotateOrigin :: Point -> Double -> Point
+rotateOrigin p theta = rotate p theta (0, 0)
+
+-- Figure is just a function telling if a point is on the figure
+
+type Figure = Point -> Bool
+
+-- Some basic figures
+
+circle :: Point -> Double -> Figure
+circle c r p = (c `euDist` p) == r
+
+disk :: Point -> Double -> Figure
+disk c r p = (c `euDist` p) < r
+
+closedDisk :: Point -> Double -> Figure
+closedDisk c r = circle c r `union` disk c r
+
+unitCircle = circle (0, 0) 1
+unitDisk = closedDisk (0, 0) 1
+flower = unionAll $ map (uncurry $ trans unitDisk) [(0, 1), (0, -1), (1, 0), (-1, 0)]
+
+-- Operations for composing figures, uses Delude to do some things nicely
+
+union :: Figure -> Figure -> Figure
+union = (||)
+
+unionAll :: [Figure] -> Figure
+unionAll = foldl union (const False)
+
+intersect :: Figure -> Figure -> Figure
+intersect = (&&)
+
+diff :: Figure -> Figure -> Figure
+diff f g = f && not g
+
+sym :: Figure -> Figure -> Figure
+sym f g = union f g `diff` intersect f g
+
+-- Operations to translate figures, just move the point and pass to original function
+-- This is a common pattern for moving figures in space
+
+trans :: Figure -> Double -> Double -> Figure
+trans f n m (x, y) = f (x - n, y - m)
+
+transHori :: Figure -> Double -> Figure
+transHori f n = trans f n 0
+
+transVert :: Figure -> Double -> Figure
+transVert f = trans f 0
+
+-- Operations to rotate figures
+
+rotateFigure :: Figure -> Double -> Point -> Figure
+rotateFigure f theta c p = f (rotate p (tau - theta) c)
+
+rotateFigureO :: Figure -> Double -> Figure
+rotateFigureO f theta = rotateFigure f theta (0, 0)
